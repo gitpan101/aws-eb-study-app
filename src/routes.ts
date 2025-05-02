55961fa3bbc254b20c0eacfa995369cfefde4e20
Ashutosh Pandey <ashpan101@gmail.com>
2025-05-02 19:56:14 +0530

    [ADD] Todo CRUD routes

--- src/routes.ts
+++ src/routes.ts
@@ -36,10 +36,17 @@ router
   .route('/todos/:userId')
   .get(async (req, res) => {
     const { userId } = req.params;
-    const result = await query('SELECT * FROM todos WHERE user_id = $1', [
-      userId,
-    ]);
 
+    const result = await query(
+      `SELECT
+       todos.id, todos.title, todos.description, todos.is_completed,
+       todos.created_at as todo_created_at, todos.user_id,
+       users.email, users.username, users.created_at as user_created_at
+       FROM todos INNER JOIN users
+       ON todos.user_id = users.id WHERE todos.user_id = $1
+       ORDER By todo_created_at`,
+      [userId],
+    );
     res.json(result.rows);
   })
   .post(async (req, res) => {
@@ -48,7 +55,7 @@ router
 
     // Validate input
     if (!title || !description) {
-      res.status(400).json({ message: 'Title and description are required' });
+      res.status(400).json({ message: 'Title and description are required.' });
       return;
     }
 
@@ -57,6 +64,39 @@ router
       [userId, title, description],
     );
     res.status(201).json(result.rows[0]);
+  });
+
+router
+  .route('/todos/:todoId')
+  .put(async (req, res) => {
+    const { todoId } = req.params;
+    const { title, description, isCompleted } = req.body;
+
+    // Validate input
+    if (!title && !description && isCompleted === undefined) {
+      res.status(400).json({
+        message: 'Title or description or isCompleted is/are required.',
+      });
+      return;
+    }
+
+    const result = await query(
+      `UPDATE todos SET title = $1, description = $2, is_completed = $3 WHERE id = $4 RETURNING *`,
+      [title, description, isCompleted, todoId],
+    );
+    res.json(result.rows[0]);
+  })
+  .delete(async (req, res) => {
+    const { todoId } = req.params;
+
+    const result = await query(`DELETE FROM todos WHERE id = $1`, [todoId]);
+
+    if (result.rowCount && result.rowCount > 0)
+      res.json('Todo deleted successfully.');
+    else
+      res.status(400).json({
+        message: 'Todo not found, no todos were deleted.',
+      });
   });
 
 export default router;

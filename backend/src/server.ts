e9063a0fd8642c01b8ce07a932456be9316bf8fb
Ashutosh Pandey <ashpan101@gmail.com>
2025-05-10 13:30:01 +0530

    [CHORE] Add build serve configs

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,7 @@
 import express, { Application } from 'express';
 import cors from 'cors';
+import path from 'path';
+
 import router from './routes';
 
 const PORT = process.env.PORT || 5000;
@@ -8,13 +10,19 @@ const app: Application = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(import.meta.dirname, '../build')));
+
 app.use('/api', router);
 // This code sets up an Express server that listens on a specified port and uses a router for API routes.
 // It also includes middleware for parsing JSON and URL-encoded data.
 
 app.get('/', (_req, res) => {
-  res.send('Welcome to the Todo API');
-  // This code defines a simple GET route for the root URL ("/") of the server.
+  res.sendFile(path.join(import.meta.dirname, '../build', 'index.html'));
+});
+
+app.get('*any', (_req, res) => {
+  res.sendFile(path.join(import.meta.dirname, '../build', 'index.html'));
 });
 
 app.listen(PORT, () => {
